d5aae43753b79bdfdc2bd81b2e0b12ab5aaf744f
pankaja92 <pankajanc@gmail.com>
2018-03-19 18:40:36 +0530

    Clean up the code

--- src/components/quote.js
+++ src/components/quote.js
@@ -5,38 +5,36 @@ import { Card,
 
 import '../App.css';
 
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const targetUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+
 class Quote extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      value : 'Quote will Post Here !',
       quote : {
         quotedesc : '',
         author : ''
       }
     };
-
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    var targetUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
-    fetch(proxyUrl+targetUrl,
-    {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept':'application/json'
-        },
-        method: "get",
-        dataType: 'json',
-    })
-    .then((res) => res.json())
-    .then((res) => {
-      var quote = {};
-      quote.author = '-  ' +res.quoteAuthor;
-      quote.quotedesc = '" '+res.quoteText+' "';
-      this.setState({quote})
-    })
-    .catch((res)=> console.log(res))
   }
+
+  componentDidMount(){
+    this.fetchQuote();
+  }
+
+  fetchQuote = async () => {
+    const res = await fetch(proxyUrl+targetUrl);
+    const data = await res.json();
+    console.log(data);
+    var quote = {};
+    quote.author = '-  ' +data.quoteAuthor;
+    quote.quotedesc = '" '+data.quoteText+' "';
+    this.setState({quote})   
+  }
+
 
   getRandomQuote = () => {
     var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -62,11 +60,17 @@ class Quote extends Component {
   }
 
   componentWillReceiveProps(nextProps){
-    console.log(nextProps.clickPass);
-    this.getRandomQuote();
+    this.fetchQuote();
   }
 
   render(){
+    if(!this.state.quote.quotedesc){
+      return(
+        <div>
+          Loading ...
+        </div>
+      )
+    }
     return (
       <div className='quoteCard'>
         <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
